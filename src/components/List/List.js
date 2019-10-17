4862056840ce29274f553e4b79fbd849603cfbf3
Jacek Olszowski <jacek.olszowski1@gmail.com>
2019-10-17 10:23:16 +0200

    add title and image props

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
+import PropTypes from 'prop-types';
 
 
 class List extends React.Component {
+    static propTypes = {
+        title: PropTypes.node.isRequired,
+        image: PropTypes.string.isRequired,
+        children: PropTypes.node, 
+    }
+    static defaultProps = {
+        children: <p>I can do all the things!!!</p>
+    }
+
   render() {
     return (
       <section className={styles.component}>
-        <Hero />
+        <Hero titleText={this.props.title} imageImg={this.props.image} />
+        <div className={styles.description}>
+          {this.props.children}
+        </div>
       </section>  
     )
   }
