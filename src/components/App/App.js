6e6918c315fc2376c9eacc742b36c5cd7687ea27
Jacek Olszowski <jacek.olszowski1@gmail.com>
2019-11-05 11:14:49 +0100

    add Home

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,30 +1,10 @@
 import React from 'react';
-import styles from './App.scss';
-import PropTypes from 'prop-types';
-import List from '../List/ListContainer.js';
-import Search from '../Search/SearchContainer.js';
+import Home from '../Home/HomeContainer';
 
-class App extends React.Component {
-  
-  static propTypes = {
-    title: PropTypes.node,
-    subtitle: PropTypes.node,
-    lists: PropTypes.array,
-  }
-
-  render() {
-    const {lists, title, subtitle} = this.props;
-    return (
-      <main className={styles.component}>
-        <h1 className={styles.title}>{title}</h1>
-        <h2 className={styles.subtitle}>{subtitle}</h2>
-        <Search />
-        {lists.map(listData => (
-          <List key={listData.id} {...listData} />
-        ))}
-      </main>
-    );
-  } 
-}
+const App = () => (
+  <div>
+    <Home />
+  </div>
+);
 
 export default App;
